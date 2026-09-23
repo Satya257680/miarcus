805a41fadd4881bdd78d8ae7ce98ac6bd42d2855
Satya257680 <nayaksatyajit372@gmail.com>
2026-09-23 18:22:21 +0530

    update layout

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -9,7 +9,6 @@ import "../../styles/theme.css";
 import ThemeProvider from "../../context/ThemeProvider";
 import LocationTrackingGate from "../LocationTrackingGate";
 import "../../styles/LocationTrackingGate.css";
-import "../../styles/miarcus-reference.css";
 
 function Layout() {
 
